d1ab454bfdef3c3744fcab05760ec5954c69819a
mylittlecorner <139819354+p1tap@users.noreply.github.com>
2024-08-19 12:48:58 +0700

    Update PriceRangeChart.js

--- src/components/Dashboard/PriceRangeChart.js
+++ src/components/Dashboard/PriceRangeChart.js
@@ -3,12 +3,14 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 
 function PriceRangeChart({ carStats }) {
   const priceRanges = [
-    { range: '0-500k', min: 0, max: 500000 },
-    { range: '500k-1M', min: 500000, max: 1000000 },
+    { range: '0-250k', min: 0, max: 250000 },
+    { range: '250k-500k', min: 250000, max: 500000 },
+    { range: '500k-750k', min: 500000, max: 750000 },
+    { range: '750k-1M', min: 750000, max: 1000000 },
     { range: '1M-1.5M', min: 1000000, max: 1500000 },
     { range: '1.5M-2M', min: 1500000, max: 2000000 },
     { range: '2M+', min: 2000000, max: Infinity }
-  ];
+];
 
   const data = priceRanges.map(range => {
     let count = 0;
